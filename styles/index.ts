a90854aaa7582b4ac13c7ce91eaae42631209467
daehwan2 <dhdh0482@naver.com>
2023-08-27 22:55:19 +0900

    Global Style 폰트적용

--- styles/index.ts
+++ styles/index.ts
@@ -1,6 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  *{
+    font-family: 'Gmarket Sans TTF';
+  }
+  
   body {
   	background: #fff;
   }
