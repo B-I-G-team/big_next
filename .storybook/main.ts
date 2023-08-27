fa971af8fd4539a3b0b3ff42b0c010245cd61a75
daehwan2 <dhdh0482@naver.com>
2023-08-27 22:43:36 +0900

    스토리북 절대 경로 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
 
 const config: StorybookConfig = {
   stories: [
@@ -20,5 +21,9 @@ const config: StorybookConfig = {
     autodocs: 'tag',
   },
   staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    (config as any).resolve.alias['@'] = path.resolve(__dirname, '../');
+    return config;
+  },
 };
 export default config;
