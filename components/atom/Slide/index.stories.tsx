b8ddf512059dbb9d8cbf08a242f08826483a23f1
daehwan2 <dhdh0482@naver.com>
2023-09-09 13:43:28 +0900

    슬라이드 컴포넌트 주석 추가

--- components/atom/Slide/index.stories.tsx
+++ components/atom/Slide/index.stories.tsx
@@ -11,6 +11,22 @@ export default {
 
 const Template: Story<Props> = (args: Props) => <Slide {...args} />;
 
+/**
+ *
+ * **사용법 예시**
+ *
+ * ```tsx
+ * import TempProfileImage from '@/public/assets/ProfileTemp.jpeg';
+ * import Temp1 from '@/public/assets/Temp1.png';
+ * ...
+ * const items=[{
+ *   image: TempProfileImage,
+ * },{
+ *   image: Temp1,
+ * }]
+ * <Slide items={items} />
+ * ```
+ */
 export const Default = Template.bind({});
 
 Default.args = {
