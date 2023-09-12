f7b1e1ec2712c4f31820d55983c147d7c412ebef
Kim minseok <80530044+AdolphKim@users.noreply.github.com>
2023-09-13 00:25:05 +0900

    Fix : 폰트 수정 (#16)
    * test
    
    * 폰트 변경
    
    * 공백 설정

--- components/atom/typography/SubTitle/index.tsx
+++ components/atom/typography/SubTitle/index.tsx
@@ -1,6 +1,7 @@
 import { Color } from '@/styles/theme';
 import { ReactChild } from 'react';
 import styled from 'styled-components';
+import TypoGraphy from '..';
 
 export interface Props {
   children: ReactChild;
@@ -13,11 +14,10 @@ const SubTitle = ({ children, color = 'text_500' }: Props) => {
 
 export default SubTitle;
 
-const Text = styled.span<{ color: Color }>`
+const Text = styled(TypoGraphy)<{ color: Color }>`
   font-family: 'Gmarket Sans TTF';
   font-size: 12px;
   font-weight: 700;
   line-height: 16px; /* 133.333% */
   letter-spacing: 0.24px;
-  color: ${({ theme, color }) => theme.color[color]};
 `;
--- styles/index.ts
+++ styles/index.ts
@@ -18,6 +18,27 @@ const GlobalStyle = createGlobalStyle`
 
     border: 1px solid ${({ theme }) => theme.color.border_100};
   }
+  @font-face {
+    font-family: 'Gmarket Sans TTF';
+    font-weight: 700;
+    src: local('Gmarket Sans TTF'),
+    url('/fonts/GmarketSansTTFBold.ttf') format('truetype');
+  }
+
+@font-face {
+    font-family: 'Gmarket Sans TTF';
+    font-weight: 500;
+    src: local('Gmarket Sans TTF'),
+    url('/fonts/GmarketSansTTFMedium.ttf') format('truetype');
+  }
+
+@font-face {
+    font-family: 'Gmarket Sans TTF';
+    font-weight: 300;
+    src: local('Gmarket Sans TTF'),
+    url('/fonts/GmarketSansTTFLight.ttf') format('truetype');
+  }
+
 `;
 
 export default GlobalStyle;
