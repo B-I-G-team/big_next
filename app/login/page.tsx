67314737905e53ea9db39476d131cee9f616b645
daehwan2 <dhdh0482@naver.com>
2023-09-27 12:39:13 +0900

    access_token 저장

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,20 @@
 'use client';
 import LoginPage from '@/components/page/LoginPage';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Index() {
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const access_token = params.get('access_token');
+
+  useEffect(() => {
+    if (access_token) {
+      localStorage.setItem('access_token', access_token);
+      router.push('/');
+    }
+  }, [access_token, router]);
+
   return <LoginPage />;
 }
